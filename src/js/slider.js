185198dba2083a28a50216da4070925b30e570af
Igor Krasylnikov <garikkrass@gmail.com>
2025-02-17 11:39:58 +0100

    added js files, modified carousel settings

---
+++ src/js/slider.js
@@ -0,0 +1,38 @@
+const swiperEl = document.querySelector('swiper-container');
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    clickable: true,
+  },
+  direction: 'vertical',
+  lazyloading: true,
+  loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+swiperEl.initialize();
